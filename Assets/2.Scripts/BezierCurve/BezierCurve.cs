d88fd28ae30847153ac452b01a7a90dff67abbea
creataek <ljslove119@naver.com>
2020-05-05 13:56:40 +0900

    .ignore 파일 수정 & namespace 설정

--- Assets/2.Scripts/BezierCurve/BezierCurve.cs
+++ Assets/2.Scripts/BezierCurve/BezierCurve.cs
@@ -2,90 +2,93 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BezierCurve : MonoBehaviour
+namespace Creataek.TheWay.Curve
 {
-    [SerializeField]
-    private LineRenderer _lineRenderer;
-
-    [SerializeField]
-    private Transform point0, point1, point2, point3;
-
-    private int _numPoints = 50;
-    private Vector3[] _positions = new Vector3[50];
-
-    private void Start()
+    public class BezierCurve : MonoBehaviour
     {
-        _lineRenderer.positionCount = _numPoints;
-    }
+        [SerializeField]
+        private LineRenderer _lineRenderer;
 
-    private void Update()
-    {
-        DrawCubicCurve();
-    }
+        [SerializeField]
+        private Transform point0, point1, point2, point3;
 
-    private void DrawLinearCurve()
-    {
-        for (int i = 1; i < _numPoints + 1; ++i)
+        private int _numPoints = 50;
+        private Vector3[] _positions = new Vector3[50];
+
+        private void Start()
         {
-            float t = i / (float)_numPoints;
-
-            _positions[i - 1] = CalculateLinearBezierPoint(t, point0.position, point1.position);
+            _lineRenderer.positionCount = _numPoints;
         }
 
-        _lineRenderer.SetPositions(_positions);
-    }
-
-    private void DrawQuadraticCurve()
-    {
-        for (int i = 1; i < _numPoints + 1; ++i)
+        private void Update()
         {
-            float t = i / (float)_numPoints;
-
-            _positions[i - 1] = CalcuateQuadraticBezierPoint(t, point0.position, point1.position, point2.position);
+            DrawCubicCurve();
         }
 
-        _lineRenderer.SetPositions(_positions);
-    }
-
-    private void DrawCubicCurve()
-    {
-        for (int i = 1; i < _numPoints + 1; ++i)
+        private void DrawLinearCurve()
         {
-            float t = i / (float)_numPoints;
+            for (int i = 1; i < _numPoints + 1; ++i)
+            {
+                float t = i / (float)_numPoints;
 
-            _positions[i - 1] = CalculateCubicBezierPoint(t, point0.position, point1.position, point2.position, point3.position);
+                _positions[i - 1] = CalculateLinearBezierPoint(t, point0.position, point1.position);
+            }
+
+            _lineRenderer.SetPositions(_positions);
         }
 
-        _lineRenderer.SetPositions(_positions);
-    }
+        private void DrawQuadraticCurve()
+        {
+            for (int i = 1; i < _numPoints + 1; ++i)
+            {
+                float t = i / (float)_numPoints;
 
-    private Vector3 CalculateLinearBezierPoint(float t, Vector3 p0, Vector3 p1)
-    {
-        // P = P0 + t(P1 – P0)
-        return p0 + t * (p1 - p0);
-    }
+                _positions[i - 1] = CalcuateQuadraticBezierPoint(t, point0.position, point1.position, point2.position);
+            }
 
-    private Vector3 CalcuateQuadraticBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
-    {
-        // B(t) = (1-t)2P0 + 2(1-t)tP1 + t2P2
-        //         uu           u        tt
-        float u = 1 - t;
-        float tt = t * t;
-        float uu = u * u;
+            _lineRenderer.SetPositions(_positions);
+        }
 
-        return (uu * p0) + (2 * u * t * p1) + (tt * p2);
-    }
+        private void DrawCubicCurve()
+        {
+            for (int i = 1; i < _numPoints + 1; ++i)
+            {
+                float t = i / (float)_numPoints;
 
-    private Vector3 CalculateCubicBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
-    {
-        // B(t) = (1-t)3 P0 + 3(1-t)2 tP1 + 3(1-t) t2 P2 + t3 P3 
-        //         uuu          uu             u   tt      ttt
-        float u = 1 - t;
-        float tt = t * t;
-        float uu = u * u;
-        float uuu = uu * u;
-        float ttt = tt * t;
+                _positions[i - 1] = CalculateCubicBezierPoint(t, point0.position, point1.position, point2.position, point3.position);
+            }
 
-        return (uuu * p0) + (3 * uu * t * p1) + (3 * u * tt * p2) + (ttt * p3);
+            _lineRenderer.SetPositions(_positions);
+        }
+
+        private Vector3 CalculateLinearBezierPoint(float t, Vector3 p0, Vector3 p1)
+        {
+            // P = P0 + t(P1 – P0)
+            return p0 + t * (p1 - p0);
+        }
+
+        private Vector3 CalcuateQuadraticBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+        {
+            // B(t) = (1-t)2P0 + 2(1-t)tP1 + t2P2
+            //         uu           u        tt
+            float u = 1 - t;
+            float tt = t * t;
+            float uu = u * u;
+
+            return (uu * p0) + (2 * u * t * p1) + (tt * p2);
+        }
+
+        private Vector3 CalculateCubicBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+        {
+            // B(t) = (1-t)3 P0 + 3(1-t)2 tP1 + 3(1-t) t2 P2 + t3 P3 
+            //         uuu          uu             u   tt      ttt
+            float u = 1 - t;
+            float tt = t * t;
+            float uu = u * u;
+            float uuu = uu * u;
+            float ttt = tt * t;
+
+            return (uuu * p0) + (3 * uu * t * p1) + (3 * u * tt * p2) + (ttt * p3);
+        }
     }
 }
--- Assets/2.Scripts/Common/PointWithVector.cs
+++ Assets/2.Scripts/Common/PointWithVector.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class PointWithVector : MonoBehaviour
+namespace Creataek.TheWay.Common
 {
-    public Vector3 dir;
-    public float magnitude;
-
-    public Vector3 Point
+    public class PointWithVector : MonoBehaviour
     {
-        get { return transform.position; }
-        set { transform.position = value; }
-    }
-    public Vector3 Vector { get { return dir * magnitude; } }
+        public Vector3 dir;
+        public float magnitude;
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawLine(Point, Point + dir * magnitude);
+        public Vector3 Point
+        {
+            get { return transform.position; }
+            set { transform.position = value; }
+        }
+        public Vector3 Vector { get { return dir * magnitude; } }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(Point, Point + dir * magnitude);
+        }
     }
 }
